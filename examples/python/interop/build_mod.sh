c2df1f27845b7c56ef6f647f95d3fb38c8279669
Jordan Jueckstock <jpjuecks@bju.edu>
2024-03-29 12:41:50 -0400

    updated profiling

--- examples/python/interop/build_mod.sh
+++ examples/python/interop/build_mod.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-gcc -fpic -shared -I/usr/include/python3.7 -ocrunchlib.so crunchlib.c
+gcc -fpic -shared $(pkg-config --cflags python3) -ocrunchlib.so crunchlib.c
 
--- examples/python/interop/crunch_profile.py
+++ examples/python/interop/crunch_profile.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+import sys
 
 def my_hash_function(buff):
     """Dumb little "hashing" function for a buffer of bytes."""
@@ -12,21 +13,23 @@ def my_hash_function(buff):
         val = ((val << 5) | (val >> 27)) & 0xffffffff
     return val
 
-if __name__ == "__main__":
-    import cProfile
-    import sys
-
+def main(argv: list[str]):
     # What file should we hash?
     try:
-        filename = sys.argv[1]
+        filename = argv[1]
     except IndexError:
         print("usage: crunch.py FILENAME")
         exit()
 
     with open(filename, 'rb') as fd:
         blob = fd.read()
-    
 
-    cProfile.run("hash_val = my_hash_function(blob)")
+    hash_val = my_hash_function(blob)
     print("{0}: 0x{1:08x}".format(filename, hash_val))
+ 
+
+if __name__ == "__main__":
+    import cProfile
+
+    cProfile.run("main(sys.argv)")
 
