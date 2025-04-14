90bedf3102f80db527446ebfd0036f028037e5e2
Jordan Jueckstock <jpjuecks@bju.edu>
2025-04-14 09:06:09 -0400

    refined library building/linking example

--- examples/c/libs/mymain.c
+++ examples/c/libs/mymain.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
+
 int add(int, int);
 
-
-int main() 
-{
-
-  int result = add(3, 2);
-  printf("Result = %d\n", result);
-
+int main() {
+	int result = add(3, 2);
+	printf("Result = %d\n", result);
+	return 0;
 }
 
--- examples/c/libs/sub.c
+++ examples/c/libs/sub.c
@@ -1,4 +1,4 @@
 int sub(int x, int y) {
-  return x + y;
+  return x - y;
 }
 
